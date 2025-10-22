b89f78e97c39d04e49b49ec017f847d5c1179850
Bestfold <143211991+Bestfold@users.noreply.github.com>
2025-10-22 12:21:10 +0200

    Lagt til prisma ORM og oppsett for CRUD i route.ts

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
+}
+
+export async function POST(req: Request) {}
+export async function PUT(req: Request) {}
+export async function DELETE(req: Request) {}
