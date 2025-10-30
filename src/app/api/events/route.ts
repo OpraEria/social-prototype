bf45f1c2389514f352d9328ee513998c67694fcc
Bestfold <143211991+Bestfold@users.noreply.github.com>
2025-10-30 12:24:27 +0100

    event_list

---
+++ src/app/api/events/route.ts
@@ -0,0 +1,45 @@
+import pool from "../../../../db.js";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const result = await pool.query(
+      `SELECT event_id, tittel, beskrivelse, lokasjon, tid, host_id
+       FROM arrangement
+	   WHERE host_id = 1
+       ORDER BY tid DESC
+       LIMIT 200`
+    );
+    return NextResponse.json(result.rows);
+  } catch (err: any) {
+    console.error("Error fetching events:", err);
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
+
+// POST: Add a new event
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const { tittel, beskrivelse, lokasjon, tid, host_id } = body;
+
+    if (!tittel || !beskrivelse || !lokasjon || !tid || !host_id) {
+      return NextResponse.json(
+        {
+          error:
+            "Missing required fields: tittel, beskrivelse, lokasjon, tid, host_id",
+        },
+        { status: 400 }
+      );
+    }
+
+    const result = await pool.query(
+      "INSERT INTO arrangement (tittel, beskrivelse, lokasjon, tid, host_id) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [tittel, beskrivelse, lokasjon, tid, host_id]
+    );
+    return NextResponse.json(result.rows[0]);
+  } catch (err: any) {
+    console.error("Failed to add user:", err);
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
